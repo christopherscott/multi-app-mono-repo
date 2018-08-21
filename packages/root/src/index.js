53e9806c1cb943bf0e81773da072c79b395a9d57
Chris Hernandez <hernandez@csdisco.com>
2018-08-21 12:18:10 -0500

    got everything working

--- packages/root/src/index.js
+++ packages/root/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/* eslint no-undef: 0 */
+import * as singleSpa from 'single-spa';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+console.log('typeof SystemJS', typeof SystemJS);
+
+let alphaPromise;
+
+singleSpa.registerApplication(
+    'test',
+    () => import(/* webpackChunkName: "test" */ './testapp.js'),
+    location => location.pathname.startsWith('/test')
+);
+
+if (process.env.NODE_ENV !== 'development') {
+    alphaPromise = import(/* webpackChunkName: "alpha", webpackPreload: true */ '@multi-app-mono-repo/alpha/build/static/js/main')
+}
+
+singleSpa.registerApplication(
+    'alpha',
+    () => {
+        if (process.env.NODE_ENV === 'development') {
+            return SystemJS.import('/local/apps/alpha'); // locally fetched via url
+        } else {
+            return alphaPromise;
+        }
+    },
+    location => location.pathname.startsWith('/alpha')
+);
+
+singleSpa.start();
