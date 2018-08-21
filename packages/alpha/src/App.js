b97e9551d5ba51301ce0bf94f5e0bf86d0f28ed7
Chris Hernandez <hernandez@csdisco.com>
2018-08-21 09:22:53 -0500

    add app-level routing

--- packages/alpha/src/App.js
+++ packages/alpha/src/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const SettingsPage = () => (
+  <div>
+    <header className="App-header">
+      <h1 className="App-title">Settings Page</h1>
+    </header>
+    <p className="App-intro">
+      TODO: add settings here
+    </p>
+    <div>
+      <Link to="/alpha">Index Page</Link>
+    </div>
+  </div>
+);
+
+const IndexPage = () => (
+  <div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to Alpha App</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <div>
+      <Link to="/alpha/settings">Settings Page</Link>
+    </div>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/alpha" component={IndexPage} />
+            <Route path="/alpha/settings" component={SettingsPage} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
